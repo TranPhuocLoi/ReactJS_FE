0403fdd803f52916fd1c3e9d65908d81c98e1308
Tran Phuoc Loi <phuocloi2203@gmail.com>
2019-04-14 13:37:00 +0700

    copy

--- Fail.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import Counter from './src/Counter'
-
-function App() {
-  return <Counter />
-}
-
-export default App;
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,70 +3,74 @@ import React from 'react'
 const getValueColor = value => {
   if (value < 0) return 'red'
   if (value > 0) return 'green'
-  return 'black'
+  return 'inherit'
 }
 
 class Counter extends React.Component {
-  //  handleIncreaseClick = () => {
-  //   const currentValue = this.state.value
-  //   this.setState({ value: currentValue + 1})
-  // }
-  //   handleDecreaseClick = () => {
-  //     const currentValue = this.state.value
-  //     this.setState({ value: currentValue - 1})
-  // }
+  state = { value: 0, delta: 0 }
 
-
-  state = { value: 1, data:0 }
-  handleClick = (delta) => {
+  handleUpdateValue = () => {
     const currentValue = this.state.value
-    return this.setState({ value: currentValue + delta})
+    this.setState({ value: currentValue + this.state.delta })
   }
-  handleClickNewVersion = (delta) => {
-    const currentData = this.state.data
-    return this.setState({ value: currentData + delta})
-  }
-  
-  handleResetClick = () => {
-    const currentValue = this.state.data
+  handleReset = () => {
+    const currentValue = this.state.value
     this.setState({ value: 0 })
   }
-  upDate = () =>{
-    return this.setState({value: currnetValue + currentData})
-  }
-  render() {
-    const value = this.state.value
-    const data = this.state.data
+  handleDeltaChange = (delta) => this.setState({ delta: this.state.delta + delta })
 
+  constructor() {
+    super();
+    this.state = { increase: 0 };
+    this._increase();
+    this.state = { decrease: 0 };
+    this._decrease();
+  }
+  
+  _increase=()=> {
+    this.setState({ increase: this.state.increase + 1 });
+    setTimeout(this._increase.bind(this), 500);
+  }
+  _decrease=()=> {
+    this.setState({ decrease: this.state.decrease - 1 });
+    setTimeout(this._decrease.bind(this), 500);
+  }
+
+  render() {
+    const { value, delta } = this.state
     return (
       <div>
         <div>
-          <div>
-            Current value: <span style={{ fontSize: '2em', color: getValueColor(value) }}>
-              {value}
-            </span>
-          </div>
-
-          <div>
-            <button style={{ backgroundColor: '#fff', border: '1px solid #000', borderRadius: '5px', padding: '5px' }} onClick={() => this.handleClick(data)}>Increase</button>
-            <button style={{ backgroundColor: '#fff', border: '1px solid #000', borderRadius: '5px', padding: '5px' }} onClick={() => this.handleClick(data)}>Decrease</button>
-            <button style={{ backgroundColor: '#fff', border: '1px solid #000', borderRadius: '5px', padding: '5px' }} onClick={this.handleResetClick}>Reset</button>
-          </div>
+          Current value: <span style={{ color: getValueColor(value) }}>
+            {value}
+          </span>
         </div>
 
         <div>
-          <div>
-            Data: <span>{data}</span>
-          </div>
-
-          <div>
-            <button style={{ backgroundColor: '#fff', border: '1px solid #000', borderRadius: '5px', padding: '5px' }} onClick={() => this.this.handleClickNewVersion(1)}>Increase</button>
-            <button style={{ backgroundColor: '#fff', border: '1px solid #000', borderRadius: '5px', padding: '5px' }} onClick={() => this.this.handleClickNewVersion(-1)}>Decrease</button>
-          </div>
+          <button onClick={this.handleUpdateValue}>
+            Update + ({value + delta})
+          </button>
+          <button onClick={this.handleReset}>
+            Reset
+          </button>
         </div>
-      </div>
-    )
-  }
-}
 
-export default Counter
+        <div>
+          Delta: <span style={{ color: getValueColor(delta) }}>{delta}</span>
+        </div>
+        <div>
+          <button onClick={() => this.handleDeltaChange(1)}>
+            Increase
+          </button>
+          <button onClick={() => this.handleDeltaChange(-1)}>
+            Decrease
+          </button>
+        </div>
+        <h1>{this.state.increase}</h1>
+        <h1>{this.state.decrease}</h1>
+      </div>
+            )
+          }
+        }
+        
+        export default Counter
